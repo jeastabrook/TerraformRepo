04000d79f20887829d22625b611672573121fa26
Jamie Eastabrook <jamie@mac.home>
2025-10-17 08:16:14 -0400

    Update VM

--- VirtualMachines/main.tf
+++ VirtualMachines/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "TEST_TERRAFORM"
+  name                = "Terraform-Test"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   size                = "Standard_B2s"

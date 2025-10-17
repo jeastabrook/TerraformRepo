cc95b384aea57ea91fbe4b279653db0fa3466f89
Jamie Eastabrook <jamie@mac.home>
2025-10-17 09:04:15 -0400

    another

--- VirtualMachines/main.tf
+++ VirtualMachines/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "Terraform-Test110"
+  name                = "Terraform-Test1100"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   size                = "Standard_B2s"
